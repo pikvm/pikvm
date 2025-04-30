b60c9a91607db6c8ac11c249cd8b37d11072c0e0
yeheshuah <yeheshuah@users.noreply.github.com>
2025-04-30 12:54:55 +0900

    Collapse/Expand section with paragraphs (#1508)

---
+++ docs/_assets/add_paragraphs.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('details').forEach(details => {
+    const summary = details.querySelector('summary');
+
+    if (!summary) return;
+
+    let linkEl;
+
+    details.addEventListener('mouseenter', () => {
+      if (details.open) return;
+
+      const summaryText = summary.textContent.trim();
+      console.log(`${summaryText}`);
+      const hash = '#' + encodeURIComponent(summaryText);
+
+      if (linkEl) return;
+
+      linkEl = document.createElement('a');
+      linkEl.href = hash;
+      linkEl.textContent = '¶';
+      linkEl.title = 'Permanent link';
+      linkEl.className = 'hoverlink';
+
+      summary.appendChild(linkEl);
+    });
+
+    details.addEventListener('mouseleave', () => {
+      if (linkEl) {
+        linkEl.remove();
+        linkEl = null;
+      }
+    });
+  });
+});
